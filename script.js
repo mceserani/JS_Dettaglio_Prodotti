b128545df48db35bfcbceae208496684d2f8a302
Matteo Ceserani <ceserani@gmail.com>
2023-05-14 23:03:05 +0200

    Complete

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 let prodotti = [];
 
-window.addEventListener('load',() => {
-    // Open the file "prodotti" and read it in the vector prodotti
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4 && xhr.status == 200) {
+// Onload, initialize the array from "prodotti.json" file
+window.onload = function(){
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState == 4 && xhr.status == 200){
             prodotti = JSON.parse(xhr.responseText);
             showProducts();
         }
-    };
-    xhr.open("GET", "prodotti", true);
+    }
+    xhr.open("GET", "prodotti.json", true);
     xhr.send();
-});
+}; 
 
-window.addEventListener('beforeunload',() => {
-    saveProducts();
-});
+// Save the array to "prodotti.json" file
+function saveProducts(){
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState == 4 && xhr.status == 200){
+            console.log("Salvataggio effettuato");
+        }
+    }
+    xhr.open("POST", "prodotti.json", true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.send(JSON.stringify(prodotti));
+};
+
 
 function addProduct(){
     // Acquire id from a prompt box
@@ -46,10 +56,27 @@ function showProducts(){
     pl.innerHTML=listaHTML;
 }
 
-function saveProducts(){
-    // Save the vector prodotti in the file "prodotti" 
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "prodotti", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify(prodotti));
+function deleteProduct(){
+    let id = prompt("Inserisci l'id del prodotto");
+    let i = 0;
+    for (let prodotto of prodotti){
+        if (id == prodotto.id){
+            prodotti.splice(i,1);
+        }
+        i++;
+    }
+    showProducts();
+}
+
+function sortProducts(){
+    prodotti.sort(function(a,b){
+        if (a.name < b.name){
+            return -1;
+        }
+        if (a.name > b.name){
+            return 1;
+        }
+        return 0;
+    });
+    showProducts();
 }
